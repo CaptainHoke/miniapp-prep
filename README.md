eb17842cbde397a2763f990eb04ffb588f63a352
CaptainHoke <70620697+CaptainHoke@users.noreply.github.com>
2024-03-12 23:36:21 +0300

    Basic server

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 ## kc.sql
 
 Tasks focused on revisiting SQL syntax that I completed during my learning on karpov.courses.
+
+## vk.go
+
+Express learning of Go for backend development.
---
+++ vk.go/http-server/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Got / request")
+	_, _ = io.WriteString(w, "Sex\n")
+}
+
+func getHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Got /hello request")
+	_, _ = io.WriteString(w, "I stole this code\n")
+}
+
+func main() {
+	http.HandleFunc("/", getRoot)
+	http.HandleFunc("/hello", getHello)
+
+	err := http.ListenAndServe(":3239", nil)
+
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Println("Server off")
+	} else if err != nil {
+		fmt.Printf("Error starting server: %s\n", err)
+		os.Exit(1)
+	}
+}
