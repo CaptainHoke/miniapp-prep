6eb3656248d4799a9c30af8b4925e0f2cbb98d97
CaptainHoke <70620697+CaptainHoke@users.noreply.github.com>
2024-03-12 23:56:29 +0300

    Multiple servers

--- vk.go/http-server/main.go
+++ vk.go/http-server/main.go
@@ -1,20 +1,28 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"io"
+	"net"
 	"net/http"
 	"os"
 )
 
+const keyServerAddress = "serverAddress"
+
 func getRoot(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Got / request")
+	ctx := r.Context()
+
+	fmt.Printf("%s: Got / request\n", ctx.Value(keyServerAddress))
 	_, _ = io.WriteString(w, "Sex\n")
 }
 
 func getHello(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Got /hello request")
+	ctx := r.Context()
+
+	fmt.Printf("%s: Got /hello request\n", ctx.Value(keyServerAddress))
 	_, _ = io.WriteString(w, "I stole this code\n")
 }
 
@@ -23,12 +31,41 @@ func main() {
 	mux.HandleFunc("/", getRoot)
 	mux.HandleFunc("/hello", getHello)
 
-	err := http.ListenAndServe(":3239", mux)
+	ctx, cancelCtx := context.WithCancel(context.Background())
+	firstServer := &http.Server{Addr: ":3239", Handler: mux, BaseContext: func(l net.Listener) context.Context {
+		ctx = context.WithValue(ctx, keyServerAddress, l.Addr().String())
+		return ctx
+	}}
+	secondServer := &http.Server{Addr: ":6969", Handler: mux, BaseContext: func(l net.Listener) context.Context {
+		ctx = context.WithValue(ctx, keyServerAddress, l.Addr().String())
+		return ctx
+	}}
 
-	if errors.Is(err, http.ErrServerClosed) {
-		fmt.Println("Server off")
-	} else if err != nil {
-		fmt.Printf("Error starting server: %s\n", err)
-		os.Exit(1)
-	}
+	go func() {
+		err := firstServer.ListenAndServe()
+
+		if errors.Is(err, http.ErrServerClosed) {
+			fmt.Println("First server off")
+		} else if err != nil {
+			fmt.Printf("Error starting first server: %s\n", err)
+			os.Exit(1)
+		}
+
+		cancelCtx()
+	}()
+
+	go func() {
+		err := secondServer.ListenAndServe()
+
+		if errors.Is(err, http.ErrServerClosed) {
+			fmt.Println("Second server off")
+		} else if err != nil {
+			fmt.Printf("Error starting second server: %s\n", err)
+			os.Exit(1)
+		}
+
+		cancelCtx()
+	}()
+
+	<-ctx.Done()
 }
