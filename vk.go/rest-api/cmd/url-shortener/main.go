f704676f6885d1e2862641921c3c3d22c8f44716
CaptainHoke <70620697+CaptainHoke@users.noreply.github.com>
2024-03-13 14:16:32 +0300

    rest-api kickstart

---
+++ vk.go/rest-api/cmd/url-shortener/main.go
@@ -0,0 +1,11 @@
+package main
+
+func main() {
+
+	// TODO: config (cleanenv)
+	// TODO: logger (slog)
+	// TODO: db (postgres)
+	// TODO: router (go-chi for now => custom impl later)
+	// TODO: run the server
+
+}
