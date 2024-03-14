abca598cd00a601db18efa549a938426dfc37426
CaptainHoke <70620697+CaptainHoke@users.noreply.github.com>
2024-03-14 20:10:58 +0300

    Reading stuff from db

--- vk.go/postgres/cmd/todo/main.go
+++ vk.go/postgres/cmd/todo/main.go
@@ -1,33 +1,59 @@
 package main
 
+// This garbage was written while following the worst fucking tutorial in existence
+
 import (
 	"context"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html/v2"
 	"github.com/jackc/pgx/v5"
 	"log"
 
 	_ "github.com/jackc/pgx/v5"
 )
 
-func createHandler(ctx *fiber.Ctx) error {
+type Todo struct {
+	Item string
+}
+
+func createHandler(ctx *fiber.Ctx, conn *pgx.Conn) error {
 	return ctx.SendString("Create")
 }
 
-func readHandler(ctx *fiber.Ctx) error {
-	return ctx.SendString("Read")
+func readHandler(ctx *fiber.Ctx, conn *pgx.Conn) error {
+	var todos []string
+
+	rows, err := conn.Query(context.Background(), "select * from items")
+	defer rows.Close()
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var row string
+	for rows.Next() {
+		err := rows.Scan(&row)
+		log.Println(row)
+		if err != nil {
+			return err
+		}
+		todos = append(todos, row)
+	}
+
+	return ctx.Render("index", fiber.Map{"Todos": todos})
 }
 
-func updateHandler(ctx *fiber.Ctx) error {
+func updateHandler(ctx *fiber.Ctx, conn *pgx.Conn) error {
 	return ctx.SendString("Update")
 }
 
-func deleteHandler(ctx *fiber.Ctx) error {
+func deleteHandler(ctx *fiber.Ctx, conn *pgx.Conn) error {
 	return ctx.SendString("Delete")
 }
 
 func main() {
 
-	dbConnectionStr := "postgresql://postgres:LemmeIn@127.0.0.1:5432/todo?sslmode=disable"
+	dbConnectionStr := "postgresql://postgres:LemmeIn@127.0.0.1:5432/todo"
 
 	conn, err := pgx.Connect(context.Background(), dbConnectionStr)
 	if err != nil {
@@ -37,21 +63,30 @@ func main() {
 		_ = conn.Close(ctx)
 	}(conn, context.Background())
 
-	/*
-		app := fiber.New()
+	engine := html.New("./views", ".html")
+	app := fiber.New(fiber.Config{Views: engine})
 
-		// TODO: config
+	// TODO: config
 
-		app.Get("/", readHandler)
-		app.Post("/", createHandler)
-		app.Put("/update", updateHandler)
-		app.Delete("/delete", deleteHandler)
+	app.Get("/", func(c *fiber.Ctx) error {
+		return readHandler(c, conn)
+	})
+	app.Post("/", func(c *fiber.Ctx) error {
+		return createHandler(c, conn)
+	})
+	app.Put("/update", func(c *fiber.Ctx) error {
+		return updateHandler(c, conn)
+	})
+	app.Delete("/delete", func(c *fiber.Ctx) error {
+		return deleteHandler(c, conn)
+	})
 
-		port := ":3239"
-		err = app.Listen(port)
+	app.Static("/", "./")
 
-		if err != nil {
-			log.Fatalln(err)
-		}
-	*/
+	port := ":3239"
+	err = app.Listen(port)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
