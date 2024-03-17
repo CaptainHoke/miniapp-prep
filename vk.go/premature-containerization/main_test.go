508110969f24e2b358415127ff973d4ebcaaad9e
CaptainHoke <70620697+CaptainHoke@users.noreply.github.com>
2024-03-17 23:32:32 +0300

    basic test setup

---
+++ vk.go/premature-containerization/main_test.go
@@ -0,0 +1,17 @@
+package main
+
+import "testing"
+
+func TestEchoArgsNoError(t *testing.T) {
+	err := echo([]string{"bin-name", "1", "2", "Ohayo"})
+	if err != nil {
+		t.Errorf("Error: %v", err)
+	}
+}
+
+func TestEchoNoArgsError(t *testing.T) {
+	err := echo([]string{"bin-name"})
+	if err == nil {
+		t.Error("Expected error, but found nil")
+	}
+}
